15fc508b8017ae2c5cb9c9819adb1d247af6d1f8
phil <phil.rice@validoc.org>
2023-03-26 22:27:52 +0200

    more tests

--- code/modules/db-auto/src/integration.spec.ts
+++ code/modules/db-auto/src/integration.spec.ts
@@ -4,6 +4,7 @@ import { readTestFile } from "@db-auto/files";
 
 const mockTestDir = testRoot + '/simple';
 
+const inCi = process.env[ 'CI' ] === 'true'
 describe ( "db-auto  makeMocks", () => {
   it ( "should make mocks", async () => {
     const expected = readTestFile ( mockTestDir, 'makeMocks.expected.txt' );
@@ -50,28 +51,50 @@ describe ( "db-auto paths", () => {
       const expected = readTestFile ( mockTestDir, 'path.driver.mission.sql.expected.txt' );
       expect ( await executeDbAuto ( mockTestDir, `driver.mission --sql` ) ).toEqual ( expected );
     } )
+    it ( "should db-auto driver.mission --fullSql", async () => {
+      const expected = readTestFile ( mockTestDir, 'path.driver.mission.fullSql.expected.txt' );
+      expect ( await executeDbAuto ( mockTestDir, `driver.mission --fullSql --page 2 --pageSize 3` ) ).toEqual ( expected );
+    } )
+
+    it ("should db-auto driver.mission --count", async () => {
+      const expected = readTestFile ( mockTestDir, 'path.driver.mission.count.expected.txt' );
+      expect ( await executeDbAuto ( mockTestDir, `driver.mission --count` ) ).toEqual ( expected );
+
+    })
+    it ("should db-auto driver.mission --distinct", async () => {
+      const expected = readTestFile ( mockTestDir, 'path.driver.mission.distinct.expected.txt' );
+      expect ( await executeDbAuto ( mockTestDir, `driver.mission --distinct --sql` ) ).toEqual ( expected );
+
+    })
+
   } )
   describe ( "execution", () => {
     it ( "should db-auto driver ", async () => {
+      if ( inCi ) return
       const expected = readTestFile ( mockTestDir, 'path.driver.expected.txt' );
       expect ( await executeDbAuto ( mockTestDir, `driver ` ) ).toEqual ( expected );
     } )
     it ( "should db-auto driver.mission ", async () => {
+      if ( inCi ) return
       const expected = readTestFile ( mockTestDir, 'path.driver.mission.expected.txt' );
       expect ( await executeDbAuto ( mockTestDir, `driver.mission` ) ).toEqual ( expected );
     } )
     it ( "should db-auto driver.mission with json output", async () => {
+      if ( inCi ) return
       const expected = readTestFile ( mockTestDir, 'path.driver.mission.expected.json' );
       expect ( await executeDbAuto ( mockTestDir, `driver.mission --json` ) ).toEqual ( expected );
     } )
     it ( "should db-auto driver.mission with no titles", async () => {
+      if ( inCi ) return
       const expected = readTestFile ( mockTestDir, 'path.driver.mission.notitles.expected.json' );
       expect ( await executeDbAuto ( mockTestDir, `driver.mission --notitles` ) ).toEqual ( expected );
     } )
     it ( "should db-auto driver.audit with oneline json output", async () => {
+      if ( inCi ) return
       const expected = readTestFile ( mockTestDir, 'path.driver.audit.expected.oneline.json' );
       expect ( await executeDbAuto ( mockTestDir, `driver.audit --onelinejson` ) ).toEqual ( expected );
     } )
+
   } )
 } )
 
@@ -104,7 +127,9 @@ describe ( "db-auto trace", () => {
   } )
 
   it ( "should build up the results - execution", async () => {
+    if ( inCi ) return
     const expected = readTestFile ( mockTestDir, 'trace.execution.expected.txt' );
     expect ( await executeDbAuto ( mockTestDir, `driver.mission.driver.audit -t` ) ).toEqual ( expected );
   } )
 } )
+
